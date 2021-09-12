80ee8da5d3177e141ac3180f3be9afa06e36ef08
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-12 16:41:46 +0900

    タイマーを追加

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t1 := time.Now()
 	fmt.Fprintf(w, "Welcome!")
+	t2 := time.Now()
+	log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
 }
 
 func main() {
-	http.HandleFunc("/",handler)
+	http.HandleFunc("/",indexHandler)
 	http.ListenAndServe(":8080",nil)
 }

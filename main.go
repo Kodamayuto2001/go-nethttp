337d2f4f46efbf9fd71700b2cb9b3c023dfb2005
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-12 16:56:26 +0900

    middlewareを追加

--- main.go
+++ main.go
@@ -3,18 +3,44 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"log"
-	"time"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	t1 := time.Now()
-	fmt.Fprintf(w, "Welcome!")
-	t2 := time.Now()
-	log.Printf("[%s] %q %v\n", r.Method, r.URL.String(), t2.Sub(t1))
+	fmt.Println("Hello, middleware!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("This is middleware test!")
+}
+
+func middleware1(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("[START] middleware1")
+		next.ServeHTTP(w, r)
+		fmt.Println("[END] middleware1")
+	}
+}
+
+func middleware2(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("[START] middleware2")
+		next.ServeHTTP(w, r)
+		fmt.Println("[END] middleware2")
+	}
+}
+
+func middleware3(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("[START] middleware3")
+		next.ServeHTTP(w, r)
+		fmt.Println("[END] middleware3")
+	}
 }
 
 func main() {
-	http.HandleFunc("/",indexHandler)
-	http.ListenAndServe(":8080",nil)
+	mux := http.NewServeMux()
+    mux.HandleFunc("/", middleware1(middleware2(middleware3(indexHandler))))
+    mux.HandleFunc("/about", middleware1(middleware2(middleware3(aboutHandler))))
+
+    http.ListenAndServe(":3000", mux)
 }

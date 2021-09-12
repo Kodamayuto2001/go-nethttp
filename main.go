03a57c4905b89eaf674ad2a552bdeada6f366692
Kodamayuto2001 <yuto2001gyokugan@gmail.com>
2021-09-12 20:38:53 +0900

    修正

--- main.go
+++ main.go
@@ -37,10 +37,29 @@ func middleware3(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
-func main() {
-	mux := http.NewServeMux()
-    mux.HandleFunc("/", middleware1(middleware2(middleware3(indexHandler))))
-    mux.HandleFunc("/about", middleware1(middleware2(middleware3(aboutHandler))))
+type middleware func(http.HandlerFunc) http.HandlerFunc
 
-    http.ListenAndServe(":3000", mux)
+type mwStack struct {
+	middlewares []middleware
+}
+
+func newMws(mws ...middleware) mwStack {
+	return mwStack{append([]middleware(nil), mws...)}
+}
+
+func (m mwStack) then(h http.HandlerFunc) http.HandlerFunc {
+	for i := range m.middlewares {
+		h = m.middlewares[len(m.middlewares)-1-i](h)
+	}
+	return h 
+}
+
+func main() {
+	middlewares := newMws(middleware1, middleware2, middleware3)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", middlewares.then(indexHandler))
+	mux.HandleFunc("/about", middlewares.then(aboutHandler))
+
+	http.ListenAndServe(":3000", mux)
 }
